adad850488c19908688c2c4a4ee35cf4c9abd8f8
Neagu Alexandru <neagu.alex0709@gmail.com>
2024-05-22 00:16:41 +0200

    aggiunto input text, aggiunto controllo con enter, aggiunto verifica testo

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,7 +1,8 @@
 var words = ["gatto","cane","calcio","pallavolo","sport","cibo","pizza"];  //7
-var spawnedWord = document.getElementsByClassName("spw-words").innerHTML;
-var text = document.getElementsByClassName("text-game").innerHTML;
 var startxt = document.getElementById("start-text");
+var rdmWord = document.getElementById("rndm-word");
+var inputValue = document.getElementById("inputWord");
+var typedWord;
 var n = Math.floor(Math.random() * words.length);
 var playing = false;
 
@@ -11,9 +12,21 @@ console.log(spawnedWord[n]);
 
 function start(){
     startxt.classList.add("hide");
-    
     playing = true;
+    game();
 }
 
+function game(){
+    if(playing == true)
+        rdmWord.textContent = spawnedWord[n];
+}
 
-
+function keyPressed(){
+    inputValue.addEventListener('keydown',(event)=>{
+        if(event.key === 'Enter'){
+            console.log('Enter key pressed!');
+            typedWord = inputValue.value;
+            console.log(typedWord);
+        }
+    });
+}
