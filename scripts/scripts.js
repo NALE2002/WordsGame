4249cd0a3232628847aac095745facc77922d164
Neagu Alexandru <neagu.alex0709@gmail.com>
2024-05-21 17:29:30 +0200

    aggiunto script per far sparire scritta start quando la si clicca

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,9 +1,17 @@
-var words = ["gatto","cane","calcio","pallavolo","sport","cibo","pizza"];  //6
+var words = ["gatto","cane","calcio","pallavolo","sport","cibo","pizza"];  //7
 var spawnedWord = document.getElementsByClassName("spw-words").innerHTML;
 var text = document.getElementsByClassName("text-game").innerHTML;
+var startxt = document.getElementById("container-start");
+var n = Math.floor(Math.random() * words.length);
 
+spawnedWord = words;
 
+console.log(spawnedWord[n]);
 
+function start(){
+    startxt.classList.add("hide");
+
+}
 
 
 
