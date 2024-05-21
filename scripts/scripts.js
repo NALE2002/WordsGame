96c2b209246a698857b301f2dc8bad49a4121c21
Neagu Alexandru <neagu.alex0709@gmail.com>
2024-05-21 19:13:31 +0200

    adesso fixati veramente :D

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,7 +1,7 @@
 var words = ["gatto","cane","calcio","pallavolo","sport","cibo","pizza"];  //7
 var spawnedWord = document.getElementsByClassName("spw-words").innerHTML;
 var text = document.getElementsByClassName("text-game").innerHTML;
-var startxt = document.getElementById("container-start");
+var startxt = document.getElementById("start-text");
 var n = Math.floor(Math.random() * words.length);
 var playing = false;
 
@@ -11,6 +11,7 @@ console.log(spawnedWord[n]);
 
 function start(){
     startxt.classList.add("hide");
+    
     playing = true;
 }
 
