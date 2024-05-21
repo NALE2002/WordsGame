361f6d1211666c44e53611cc83488c7fccdeac55
Neagu Alexandru <neagu.alex0709@gmail.com>
2024-05-21 15:34:19 +0200

    aggiunto start in mezzo al container

---
+++ scripts/scripts.js
@@ -0,0 +1,9 @@
+var words = ["gatto","cane","calcio","pallavolo","sport","cibo","pizza"];  //6
+var spawnedWord = document.getElementsByClassName("spw-words").innerHTML;
+var text = document.getElementsByClassName("text-game").innerHTML;
+
+
+
+
+
+
