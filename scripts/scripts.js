c55a6345c27b0e881e8fc713f2e51864a9b0baa8
Neagu Alexandru <neagu.alex0709@gmail.com>
2024-05-26 18:44:03 +0200

    fixata funzioni generateWord() e startGame()

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,39 +1,62 @@
 var words = ["gatto","cane","calcio","pallavolo","sport","cibo","pizza"];  //7
 var startxt = document.getElementById("start-text");
 var inputValue = document.getElementById("inputWord");
-var p = document.createElement("p");
-var spawner = document.getElementById("wordSpawner").append(p);
-var typedWord;
-var n = Math.floor(Math.random() * words.length);
-var nrow = Math.floor(Math.random() * 10); 
+var wordSpawner = document.getElementById("wordSpawner");
+// var p = document.createElement("p");
+var typedWord; 
 var playing = false;
-var hearts = 3;
-spawnedWord = words;
+var intervalID;
 
 
-function start(){
+function startGame(){
     startxt.classList.add("hide");
     playing = true;
-    generateWord();
+    inputValue.focus();
+    gameLoop();
+    inputValue.addEventListener('keydown',handleEvent);
 }
 
 function generateWord(){
-    if(playing == true){
-        p;
-        p.classList.add("spwn-words");
-        p.style.gridRowStart = nrow;
-        p.textContent = spawnedWord[n];
-        console.log(p);
+    var p = document.createElement("p");
+    p.classList.add("spwn-words");
+    p.style.gridRowStart = randomRow();
+    p.textContent = randomWord();
+    wordSpawner.appendChild(p);
+    
+    setTimeout(() => {
+        if (wordSpawner.contains(p)) {
+            wordSpawner.removeChild(p);
+        }
+    }, 4000); // Rimuove la parola dopo 4 secondi (tempo dell'animazione)
+}
+
+function handleEvent(event){
+    if(event.key === 'Enter' && playing){
+        inputValue = '';
+
+
     }
 }
 
-function keyPressed(){
-    inputValue.addEventListener('keydown',(event)=>{
-        if(event.key === 'Enter' && playing == true){
-            console.log('Enter key pressed!');
-            typedWord = inputValue.value;
-        }
-    });
+function checkTypedWord(word){
+    if(typedWord === word)
+        
 }
 
+function randomWord(){
+    var n = Math.floor(Math.random() * words.length);
+    return spawnedWord[n];
+}
+
+function randomRow(){
+    var nrow = Math.floor(Math.random() * 10);
+    return nrow;
+}
+
+function gameLoop(){
+    if(playing){
+        generateWord();
+        setTimeout(gameLoop(),2000);
+    }
+}
 
