4a5112188d904ba0311adf1c28e56d81ffd5d021
Neagu Alexandru <neagu.alex0709@gmail.com>
2024-05-27 03:15:36 +0200

    aggiunto sistema di input con funzione handleEvent()

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,60 +1,74 @@
 var words = ["gatto","cane","calcio","pallavolo","sport","cibo","pizza"];  //7
-var startxt = document.getElementById("start-text");
-var inputValue = document.getElementById("inputWord");
-var wordSpawner = document.getElementById("wordSpawner");
-// var p = document.createElement("p");
-var typedWord; 
-var playing = false;
-var intervalID;
-var hp = 3;
+var startxt = document.getElementById("start-text"); //"start" text at the beginning of the page/game
+var inputValue = document.getElementById("inputWord"); //input for the words to write
+var wordSpawner = document.getElementById("wordSpawner"); //container of the words
+var starth2 = document.getElementById("starth2");
+var typedWord; //typed word XD
+var playing = false; //current state of the game: if its currently playing or not
+var intervalID; //decides how many seconds for a new word
+var countDown; //countdown after clkicking the "start"
+var hp = 3; //how many lifes you have before losing
+
+//aggiungere sistema input 
 
 
 function startGame(){
-    startxt.classList.add("hide");
+    startxt.removeChild(starth2);
     playing = true;
     inputValue.focus();
     gameLoop();
-    endGame();
     inputValue.addEventListener('keydown',handleEvent);
+
 }
 
 function gameLoop(){
     if(playing){
         generateWord();
-        intervalID = setTimeout(gameLoop(),2000);
+        intervalID = setTimeout(gameLoop,2000); //generates a word every 2 seconds
     }
-}
-
-function generateWord(){
-    var p = document.createElement("p");
-    p.classList.add("spwn-words");
-    p.style.gridRowStart = randomRow();
-    p.textContent = randomWord();
-    wordSpawner.appendChild(p);
-    
-    setTimeout(() => {
-        if (wordSpawner.contains(p)) {
-            wordSpawner.removeChild(p);
-            hp--;
-        }
-    }, 4000); // Rimuove la parola dopo 4 secondi (tempo dell'animazione)
+    console.log("current hp: ",hp);
+    endGame();
 }
 
 function handleEvent(event){
     if(event.key === 'Enter' && playing){
-        inputValue = '';
+        typedWord = inputValue.value; 
+        console.log("parola scritta :", typedWord);
+    }
+}
+    
 
 
+function generateWord(){
+    var p = document.createElement('p');
+    p.classList.add("spwn-words");
+    p.style.gridRowStart = randomRow();
+    p.textContent = randomWord();
+    wordSpawner.appendChild(p); //creates a <p> tag containing a random word and row inside the container for words
+
+    setTimeout(() => {
+        if(wordSpawner.contains(p)){
+            wordSpawner.removeChild(p);
+            hp--;
+        }
+    }, 4000); //cancella parola dopo 4 secondi
+    
+}
+
+function endGame(){
+    if(hp === 0 && playing){
+        playing = false;
+        while(wordSpawner.firstChild){
+            wordSpawner.firstChild.remove();
+        }
+        var gameOver = document.createElement('h2');
+        gameOver.classList.add("info-text");
+        gameOver.textContent = 'Game Over :C';
+        startxt.appendChild(gameOver);
     }
 }
 
-function checkTypedWord(word){
-    // if(typedWord === word)
-
-        
-}
-
-function randomWord(){
+function randomWord(){ 
     var n = Math.floor(Math.random() * words.length);
     return words[n];
 }
@@ -62,15 +76,4 @@ function randomWord(){
 function randomRow(){
     var nrow = Math.floor(Math.random() * 10) + 1;
     return nrow;
-}
-
-function endGame(){
-    if(hp == 0){ 
-        while(wordSpawner.firstChild){
-            wordSpawner.firstChild.remove();
-        }
-        playing = false;
-        clearTimeout(intervalId);
-        inputValue.removeEventListener('keydown', handleKeyPress);
-    }
 }
